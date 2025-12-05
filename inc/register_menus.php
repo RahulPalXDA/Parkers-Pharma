cf32fe27b8f990b669de4bc2b22bf5d5a1709e71
RahulPalXDA <RahulPal.Inbox@gmail.com>
2025-12-05 17:09:33 +0530

    feat: Add new hero and help background images and implement theme internationalization with 'parkers-pharma' text domain.

--- inc/register_menus.php
+++ inc/register_menus.php
@@ -3,9 +3,9 @@ function mytheme_register_menus()
 {
     register_nav_menus(
         array(
-            'primary' => __('Header Menu'),
-            'footer' => __('Quick Links Menu'),
-            'social' => __('Social media Menu'),
+            'primary' => __('Header Menu', 'parkers-pharma'),
+            'footer' => __('Quick Links Menu', 'parkers-pharma'),
+            'social' => __('Social media Menu', 'parkers-pharma'),
         )
     );
 }
